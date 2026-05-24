cdba5fe22d6d3d41f3965139dc7324ec26d82522
Mostafa Gheta <mostafagheta75@gmail.com>
2026-05-24 04:47:24 +0300

    K8S added

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,6 +1,10 @@
 -- ./mysql-init/init.sql
 CREATE DATABASE IF NOT EXISTS weatherapp;
-CREATE USER 'authuser'@'%' IDENTIFIED BY 'my-secret-pw';
-GRANT ALL PRIVILEGES ON weatherapp.* TO 'authuser'@'%';
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    user_password VARCHAR(128)
+);
+GRANT ALL PRIVILEGES ON weatherapp.* TO 'admin'@'%';
 FLUSH PRIVILEGES;
-
